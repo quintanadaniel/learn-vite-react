fdccc92f643c3d89bc2c0450e79276d1e2edde72
Daniel Quintana <quintana.danielarmando@gmail.com>
2023-03-01 20:51:40 +0000

    create first project twitter component

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import { TwitterFollowCard } from "./components/TwitterFollowCard";
+
+const users = [
+  {
+    userName: "midudev",
+    name: "Miguel Angel Duran",
+    isFollowing: true,
+  },
+  {
+    userName: "pheralb",
+    name: "Pedro Hernandez",
+    isFollowing: false,
+  },
+  {
+    userName: "PacoHdezs",
+    name: "Paco Hdez",
+    isFollowing: true,
+  },
+  {
+    userName: "TMChein",
+    name: "Tomas",
+    isFollowing: false,
+  },
+];
+export function App() {
+  return (
+    <section className="App">
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard
+          key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        );
+      })}
+    </section>
+  );
+}
