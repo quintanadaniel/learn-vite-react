69d39de44d80d3810c4e3569b5761cf5843fa1e0
Daniel Quintana <quintana.danielarmando@gmail.com>
2023-03-02 09:29:52 +0000

    create third project mouse click

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("efecto", { enabled });
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+    // limpiamos el el evento montado, y desmontamos un componente, podmos hacer un return en el useEffect
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+};
+
+function App() {
+  return <FollowMouse />;
+}
+
+export default App;
